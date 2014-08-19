48471dfe5e0577a7ab0c348c2f3c192c35bcd5ab
Pradeep <pradeep@xaptum.com>
2014-08-18 19:43:48 -0500

    OPEN - task 2: Submit code for C/C++ https://github.com/bladeninja/zerosocket/issues/issues/issue/2

--- libzerosocket-cpp/src/zerosocket/RBTree.cpp
+++ libzerosocket-cpp/src/zerosocket/RBTree.cpp
@@ -1,26 +1,12 @@
 /********************************************************************************
- * XAPTUM CONFIDENTIAL
- * ________________________________________________________________
- * 
- *  [2012] - [2014] Xaptum Incorporated 
- *  All Rights Reserved.
- * 
- * NOTICE:  All information contained herein is, and remains
- * the property of Xaptum Incorporated and its suppliers,
- * if any.  The intellectual and technical concepts contained
- * herein are proprietary to Xaptum Incorporated
- * and its suppliers and may be covered by U.S. and Foreign Patents,
- * patents in process, and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from Xaptum Incorporated.
- * 
- * /libzerosocket-cpp/RBTree.cpp
- * RBTree.cpp
- * 
- * Created on: Aug 15, 2014
- *     Author: pradeepbarthur Inc.
+ * Copyright (C) 2014-2014 Zero Socket
+ * based on code by Pradeep Barthur copyright (C) 2014-2014 Zero Socket
+ * License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ *
+ * Created on:Aug 13, 2014
+ *     Author: Pradeep Barthur
  ********************************************************************************/
+
 #include <stdio.h>
 #include <limits.h>
 #include <RBTree.h>

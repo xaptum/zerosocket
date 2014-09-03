31c8eb179a86b3db84499a285d9c8bb9fcecf871
Pradeep <pradeep@xaptum.com>
2014-09-02 19:37:11 -0500

    updated to support ssl in test

--- libzerosocket-py/src/test/test_zsocket.py
+++ libzerosocket-py/src/test/test_zsocket.py
@@ -4,13 +4,15 @@ Created on Sep 2, 2014
 @author: pradeepbarthur
 '''
 import socket
+import ssl
 from zsocket import (zsocket,FileConfig)
 
 def main():
     rsock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    ssock = ssl.wrap_socket(rsock, cert_reqs=ssl.CERT_NONE)
     conf = FileConfig.FileConfig('sock.conf')
     
-    sock = zsocket.zsocket(conf,rsock)
+    sock = zsocket.zsocket(conf,ssock)
     sock.connect()
     sock.send("hello world")
     
